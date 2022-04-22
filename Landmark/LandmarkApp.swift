6aaf3b14dcc69b95448d334e2122183dc2c025c6
didik <didik.maryono@pawoon.com>
2022-04-22 22:32:06 +0700

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Didik Maryono on 22/04/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
